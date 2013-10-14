3927b4bfd5a6f5e6c1cb5744704f194eb4125df9
Felipe Vieira <nvieirafelipe@gmail.com>
2013-10-14 13:14:03 -0300

    Use issue package to retrieve issues for each milestone

--- src/github.com/nvieirafelipe/scorch/milestone/milestone.go
+++ src/github.com/nvieirafelipe/scorch/milestone/milestone.go
@@ -1,7 +1,9 @@
 package milestone
 
 import (
+  "strconv"
   "github.com/nvieirafelipe/go-github/github"
+  "github.com/nvieirafelipe/scorch/issue"
 )
 
 type MilestoneCollection struct {
@@ -9,16 +11,19 @@ type MilestoneCollection struct {
 }
 
 type Milestone struct {
+  Number      int               `json:"number"`
   Title       string            `json:"title"`
   URL         string            `json:"url"`
   CreatedAt   github.Timestamp  `json:"created_at"`
   DueOn       github.Timestamp  `json:"due_on"`
+  Issues      []issue.Issue           `json:"issues"`
 }
 
-func MilestonesFromGithub(githubMilestones []github.Milestone) MilestoneCollection {
+func MilestonesFromGithub(githubMilestones []github.Milestone, githubClient *github.Client, organization string, repository string) MilestoneCollection {
   milestones := make([]Milestone, 0)
   for _, milestone := range githubMilestones {
-    milestones = append(milestones, Milestone{URL: *milestone.URL, Title: *milestone.Title, CreatedAt: *milestone.CreatedAt, DueOn: *milestone.DueOn})
+    issues := issue.IssuesFromMilestone(githubClient, strconv.Itoa(*milestone.Number), organization, repository)
+    milestones = append(milestones, Milestone{Number: *milestone.Number, URL: *milestone.URL, Title: *milestone.Title, CreatedAt: *milestone.CreatedAt, DueOn: *milestone.DueOn, Issues: issues})
   }
   return MilestoneCollection{Milestones: milestones}
 }
