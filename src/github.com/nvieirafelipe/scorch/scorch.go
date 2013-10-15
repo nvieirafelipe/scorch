f0b14861605c2fc18b25b6c3fb3f934fdceef9dd
Felipe Vieira <nvieirafelipe@gmail.com>
2013-10-15 18:51:55 -0300

    Serves pages and static files under public/ folder

--- src/github.com/nvieirafelipe/scorch/scorch.go
+++ src/github.com/nvieirafelipe/scorch/scorch.go
@@ -4,6 +4,7 @@ import (
   "net/http"
   "os"
   "log"
+  "strings"
   "github.com/bmizerany/pat"
   "github.com/nvieirafelipe/scorch/organization"
   "github.com/nvieirafelipe/scorch/repository"
@@ -13,6 +14,9 @@ func main() {
   routes := pat.New()
   routes.Get("/organizations/:organization_name/repositories", http.HandlerFunc(organization.Repositories))
   routes.Get("/repositories/:repository_name/workleft-vs-time", http.HandlerFunc(repository.WorkLeftVSTime))
+  routes.Get("/public/", http.HandlerFunc(public))
+  routes.Get("/", http.HandlerFunc(home))
+  routes.Get("/:page", http.HandlerFunc(pages))
 
   http.Handle("/", routes)
   log.Println("listening...")
@@ -21,4 +25,20 @@ func main() {
   if err != nil {
     log.Fatal("ListenAndServe: ", err)
   }
+}
+
+func public(w http.ResponseWriter, r *http.Request) {
+  http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+  log.Println("home")
+  http.ServeFile(w, r, r.URL.Path[1:]+"public/index.html")
+}
+
+func pages(w http.ResponseWriter, r *http.Request) {
+  page := r.URL.Query().Get(":page")
+  page = strings.Split(page, ".")[0]
+  log.Println("pages %v", page)
+  http.ServeFile(w, r, "public/"+page+".html")
 }
